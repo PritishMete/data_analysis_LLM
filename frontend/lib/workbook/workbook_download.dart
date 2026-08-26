dc00364136dfadc35ebba480161ec19982d72378
PritishMete <jaiphotoshoot@gmail.com>
2026-08-26 10:23:23 +0530

    Clear repository

--- frontend/lib/workbook/workbook_download.dart
+++
@@ -1,2 +0,0 @@
-export 'workbook_download_stub.dart'
-    if (dart.library.html) 'workbook_download_web.dart';
--- frontend/lib/workbook/workbook_download_stub.dart
+++
@@ -1,5 +0,0 @@
-import 'dart:typed_data';
-
-void downloadWorkbookBytes(Uint8List bytes, String fileName) {
-  // No-op on non-web test environments.
-}
